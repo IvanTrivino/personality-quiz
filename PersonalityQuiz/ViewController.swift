eede61a3d7a192864c5d21af9d4f9a6df5c9514a
Ivan Camilo Triviño López <ivancamilo.trivi@icloud.com>
2018-12-17 10:06:05 -0500

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Ivan Camilo Triviño López on 12/17/18.
+//  Copyright © 2018 Ivan Camilo Triviño López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
